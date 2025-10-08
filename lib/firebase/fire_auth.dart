8225b3312d3c699f2d848841c1f92472afb841ce
CaracheoAdan <21031400@itcelaya.edu.mx>
2025-10-08 15:11:39 -0700

    Verificacion de email

--- lib/firebase/fire_auth.dart
+++ lib/firebase/fire_auth.dart
@@ -12,7 +12,28 @@ class FireAuth {
           email: email, password: password);
       User? user = result.user;
       user!.sendEmailVerification(); //envia correo de verificacion
-      
+
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }  
+  Future<User?> signInWithEmailAndPassword(// devuelve un objeto
+      String email, String password) async {
+        //recibe email y password(2parametros)
+    try {
+      //Creacion de un objeto con las credenciales, si es correcto
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      if(user != null && !user.emailVerified){
+        await _auth.signOut();
+        throw FirebaseAuthException(
+          code: 'email-not-verified',
+          message: 'Please verify your email before signing in.'
+        );
+      }
       return user;
     } catch (e) {
       print(e.toString());
@@ -20,3 +41,4 @@ class FireAuth {
     }
   }  
 }
+
